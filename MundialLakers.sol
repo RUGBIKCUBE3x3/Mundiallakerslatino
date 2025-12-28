521a6920b092a823ff91e24833e85614b579d29f
RUGBIKCUBE3x3 <alejandroslimsoumaya@gmail.com>
2025-12-27 23:29:03 -0600

    Create MundialLakers.sol

---
+++ MundialLakers.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+contract MundialLakers { 
+    string public proyecto = "Torre Latimo 2026";  
+    string public luz = "Lakers Gold";
+
+    function cambiarLuz() public {
+        luz = "Lakers Purple";
+    }
+}
